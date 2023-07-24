6ada69e3b11561e004fb4328f8bbe91a1a737d6b
Yann Kamche <yannkamche@gmail.com>
2023-07-24 13:28:42 +0100

    completed the home section, styling and added js functions

--- js/script.js
+++ js/script.js
@@ -5,4 +5,23 @@ let navbar = document.querySelector('.header .navbar');
 menu.onclick = () => {
     menu.classList.toggle('fa-times');
     navbar.classList.toggle('active');
-}
+};
+
+// -----***** REMOVE MENU AND NAVBAR ON SCROLL
+window.onscroll = () => {
+    menu.classList.remove('fa-times');
+    navbar.classList.remove('active');
+};
+
+// ----**** INITIALIZE SWIPER***------
+var swiper = new Swiper(".home-slider", {
+    spaceBetween: 20,
+    effect: "fade",
+    grabCursor: true,
+    loop:true,
+    centeredSlides: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+});
