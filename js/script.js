5822da893547762e9c99458efc51c37a09235147
Yann Kamche <yannkamche@gmail.com>
2023-08-05 19:05:52 +0100

    Added swiper to the footer

--- js/script.js
+++ js/script.js
@@ -41,3 +41,28 @@ var swiper = new Swiper(".review-slider", {
         },
     },
 });
+
+var swiper = new Swiper(".blogs-slider", {
+    spaceBetween: 20,
+    grabCursor: true,
+    loop:true,
+    autoplay: {
+        delay: 7500,
+        disableOnInteraction: false,
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    breakpoints:{
+        0:{
+            slidesPerView:1,
+        },
+        768:{
+            slidesPerView:2,
+        },
+        991:{
+            slidesPerView:3,
+        },
+    },
+});
